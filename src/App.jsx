eeac9e3473f955389fccd1defa420f2029f2ac92
murarigupta <murarigupta28598@gmail.com>
2025-01-02 17:13:32 +0530

    added all the dependencies and created Footer Components in the LMS frontEnd Project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Footer from './Components/Footer'
+
+function App() {
+  
+
+  return (
+   <>
+   {/* LMS FrontEnd Part
+   <h1 className='text-lg text-yellow-800 font-mono bg-gray-500'>Checking the tailwind</h1> */}
+   <Footer />
+   </>
+  )
+}
+
+export default App
