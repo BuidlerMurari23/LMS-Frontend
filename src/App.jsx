71396bddb61342fdbff59769a8e9e5e40bc4851c
murarigupta <murarigupta28598@gmail.com>
2025-01-12 20:36:13 +0530

    Login page has been successfully autheticated

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 
-import HomePage from './Pages/HomePage'
-import AboutUs from './Pages/AboutUs'
-import NotFound from './Pages/NotFound'
-import Signup from './Pages/Signup'
+import HomePage from './Pages/HomePage.jsx'
+import AboutUs from './Pages/AboutUs.jsx'
+import NotFound from './Pages/NotFound.jsx'
+import Signup from './Pages/Signup.jsx'
+import Login from './Pages/Login.jsx'
 
 function App() {
   
@@ -16,6 +17,7 @@ function App() {
       <Route path='/' element={<HomePage />} />
       <Route path='/about' element={<AboutUs />} />
       <Route path='/signup' element={<Signup />} />
+      <Route path='/login' element={<Login />} />
       <Route path='*' element={<NotFound />} />
    </Routes>
    </>
